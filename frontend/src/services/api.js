1df6c9456488c4fe356096224d60cfb925b6ebdd
Deniska2525 <denis.tuboltsev@mail.ru>
2026-04-03 00:07:40 +0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-// В Docker контейнере API доступен через прокси Nginx по пути /api/
-// Для локальной разработки используем localhost
-const API_URL = import.meta.env.PROD ? '/api' : 'http://localhost:8000'
+const API_URL = import.meta.env.PROD 
+    ? import.meta.env.VITE_API_URL  // берем из переменной
+    : 'http://localhost:8000'
 
 const api = axios.create({
-  baseURL: API_URL,
-  headers: { 'Content-Type': 'application/json' }
+    baseURL: API_URL,
+    headers: { 'Content-Type': 'application/json' }
 })
 
 api.interceptors.request.use(config => {
