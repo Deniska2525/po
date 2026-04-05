624532b7ed3dc0cdaec284a08a13a59eb083bac5
Deniska2525 <denis.tuboltsev@mail.ru>
2026-04-05 13:44:36 +0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API_URL,
     headers: { 'Content-Type': 'application/json' }
 })
 
